509cac6cacf85fd14525d054e8b501c11ff07a87
Joshua Hull <jhull@ccihealth.org>
2016-07-14 11:20:39 -0400

    Added Set and Updated __init__.py

---
+++ pokemonsdk/set.py
@@ -0,0 +1,21 @@
+import json
+from pokemonsdk.querybuilder import QueryBuilder
+from pokemonsdk.config import __endpoint__
+
+
+class Set(object):
+    RESOURCE = 'sets'
+
+    def __init__(self, response_dict={}):
+        self.code = response_dict.get('code')
+        self.name = response_dict.get('name')
+        self.series = response_dict.get('series')
+        self.total_cards = response_dict.get('totalCards')
+        self.standard_legal = response_dict.get('standardLegal')
+        self.release_date = response_dict.get('releaseDate')
+
+    def where(self, **kwargs):
+        return QueryBuilder(Set).where(**kwargs)
+
+    def all(self):
+        return QueryBuilder(Set).all()
